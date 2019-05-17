ab8237948af202d4b11396bb4a22a6521126efab
lql <FreshStudent@users.noreply.github.com>
2019-05-17 18:30:45 +0800

    day3 finish

---
+++ day04/day04_01.py
@@ -0,0 +1,14 @@
+﻿"""
+for-in 循环 实现1~100 求和
+
+version 0.1
+autho lql
+date 2019-05-17
+
+"""
+sum = 0
+for x in range(101):
+    sum += x
+print(sum)
+
+
---
+++ day04/day04_02.py
@@ -0,0 +1,21 @@
+﻿"""
+for循环实现100内的偶数求和
+
+version 0.1
+author lql
+
+"""
+
+sum = 0
+for x in range(0,100,2):
+    sum = sum+x
+print(sum)
+
+"""
+另外一种实现方式
+"""
+sum = 0
+for x in range(0,100):
+    if x%2 ==0:
+        sum +=x
+    print(sum)
